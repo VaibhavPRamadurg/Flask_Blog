f49dd58ecb54598a913fda9a9479b8cc68ba542e
VaibhavPRamadgurg <vaibhav200704@gmail.com>
2026-06-05 09:44:31 +0530

    migration: switch production database to postgresql

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,6 +3,6 @@ import os
 
 class config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI =os.getenv('DATABASE_URL', 'sqlite:///site.db')
     RESEND_API_KEY = os.environ.get('RESEND_API_KEY')
     MAIL_DEFAULT_SENDER = os.getenv('EMAIL_USER') 
--- run.py
+++ run.py
@@ -3,7 +3,5 @@ from flaskblog import create_app
 
 app=create_app()
 
-
-
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
